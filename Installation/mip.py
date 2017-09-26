841f3233dabf759fff87dedbcef684315e7f4681
wurmen <ftcu5931@gmail.com>
2017-09-26 22:35:40 +0800

    Create mip.py

---
+++ Installation/mip.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+# Copyright 2016, Gurobi Optimization, Inc.
+
+# This example formulates and solves the following simple MIP model:
+#  maximize
+#        x +   y + 2 z
+#  subject to
+#        x + 2 y + 3 z <= 4
+#        x +   y       >= 1
+#  x, y, z binary
+
+from gurobipy import *
+
+try:
+
+    # Create a new model
+    m = Model("mip1")
+
+    # Create variables
+    x = m.addVar(vtype=GRB.BINARY, name="x")
+    y = m.addVar(vtype=GRB.BINARY, name="y")
+    z = m.addVar(vtype=GRB.BINARY, name="z")
+
+    # Integrate new variables
+    m.update()
+
+    # Set objective
+    m.setObjective(x + y + 2 * z, GRB.MAXIMIZE) 
+
+    # Add constraint: x + 2 y + 3 z <= 4
+    m.addConstr(x + 2 * y + 3 * z <= 4, "c0")
+
+    # Add constraint: x + y >= 1
+    m.addConstr(x + y >= 1, "c1")
+
+    m.optimize()
+    m.write('mip1.lp')
+
+    for v in m.getVars():
+        print('%s %g' % (v.varName, v.x))
+
+    print('Obj: %g' % m.objVal)
+
+except GurobiError:
+    print('Encountered a Gurobi error')
