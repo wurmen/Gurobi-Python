11427a6915e7c5bfda756b957c56f71567ac58d9
Cheng-Man Wu <ftcu5931@gmail.com>
2018-02-26 15:11:15 +0800

    Update Optimal Employee Work Schedule.py

--- python-gurobi  model/Optimal Employee Work Schedule.py
+++ python-gurobi  model/Optimal Employee Work Schedule.py
@@ -19,22 +19,22 @@ staffNumber={}
 m=Model("work_schedule")
 
 for d in employee:
-for i in range(t):
-    for j in range(i+1,t+1):
-        x[d,i,j]=m.addVar(vtype=GRB.BINARY,name="x_%s_%d_%d"%(d,i,j))
+    for i in range(t):
+        for j in range(i+1,t+1):
+            x[d,i,j]=m.addVar(vtype=GRB.BINARY,name="x_%s_%d_%d"%(d,i,j))
 for c in range(t):
-staffNumber[c]=m.addVar(vtype=GRB.INTEGER,lb=required[c],name='staffNumber_%d'%c)
+    staffNumber[c]=m.addVar(vtype=GRB.INTEGER,lb=required[c],name='staffNumber_%d'%c)
 
 m.update()
 
 m.setObjective(quicksum(quicksum(quicksum((j-i)*x[d,i,j]*cost[d] for j in range(i+1,t+1))for i in range(t))for d in employee),GRB.MINIMIZE)
 
 for d in employee:
-m.addConstr(quicksum(quicksum(x[d,i,j] for j in range(i+1,ave[d]+1)if min[d] <= (j-i) <= max[d])for i in range(avs[d],ave[d]))<=1)
-m.addConstr(quicksum(quicksum(x[d,i,j] for j in range(i+1,t+1))for i in range(t))<=quicksum(quicksum(x[d,i,j] for j in range(i+1,ave[d]+1)
+    m.addConstr(quicksum(quicksum(x[d,i,j] for j in range(i+1,ave[d]+1)if min[d] <= (j-i) <= max[d])for i in range(avs[d],ave[d]))<=1)
+    m.addConstr(quicksum(quicksum(x[d,i,j] for j in range(i+1,t+1))for i in range(t))<=quicksum(quicksum(x[d,i,j] for j in range(i+1,ave[d]+1)
 if min[d] <= (j-i) <= max[d])for i in range(avs[d],ave[d])))
 for c in range(t):
-m.addConstr(quicksum(quicksum(quicksum(x[d,i,j] for j in range(i+1,t+1)if i <= c <j) for i in range(t))for d in employee)==staffNumber[c])
+    m.addConstr(quicksum(quicksum(quicksum(x[d,i,j] for j in range(i+1,t+1)if i <= c <j) for i in range(t))for d in employee)==staffNumber[c])
 D101=quicksum(quicksum(x['ANDERSON',i,j] for j in range(i+1,7)if min["ANDERSON"]<=(j-i)<=max["ANDERSON"])for i in range(0,7))
 D102=quicksum(quicksum(x['ANDERSON',i,j] for j in range(i+1,21))for i in range(18,21))
 m.addConstr(D101+D102<=1,"F")
@@ -45,26 +45,13 @@ m.write("work_schedule.lp")
 
 print ("Optimal objective value is %g"%m.objVal)
 if m.status == GRB.Status.OPTIMAL:
-solution = m.getAttr('x', x)
-for d in employee :
-    for i in range(t):
-        for j in range(i+1,t+1):
-            if solution[d,i,j] == 1:
-                print("The working time of %s is from %g to %g" % (d,i,j))
+    solution = m.getAttr('x', x)
+    for d in employee :
+        for i in range(t):
+            for j in range(i+1,t+1):
+                if solution[d,i,j] == 1:
+                    print("The working time of %s is from %g to %g" % (d,i,j))
 
-staffNumber_sol=m.getAttr('x',staffNumber)
-for c in range(t):
-    print 'The number of staff from %d -%d: %d'%(c,c+1,staffNumber_sol[c])
-        
-
-
-
-
-
-
-
-
-
-
-
-
+    staffNumber_sol=m.getAttr('x',staffNumber)
+    for c in range(t):
+        print 'The number of staff from %d -%d: %d'%(c,c+1,staffNumber_sol[c])
